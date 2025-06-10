146018f36730f1d532840ac6dda872d152c2dbf3
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-06-10 22:41:45 +0330

    html form added

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html")
 
 
 app.run(debug=True, port=5001)
